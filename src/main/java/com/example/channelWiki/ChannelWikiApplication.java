a7f4f415a4ea4a2d9c3482551526c8aefcc75cca
syeeuns <syeeuns@gmail.com>
2022-01-16 16:47:05 +0900

    init

---
+++ src/main/java/com/example/channelWiki/ChannelWikiApplication.java
@@ -0,0 +1,12 @@
+package com.example.channelWiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelWikiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/channelWiki/ChannelWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.channelWiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannelWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
